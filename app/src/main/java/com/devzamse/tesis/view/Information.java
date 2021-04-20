44763b68d5ca7fc8c657cd7f9e6800cba4722102
devzamse <nilovila@e-quipu.pe>
2021-04-20 02:25:17 -0500

    avances

---
+++ app/src/main/java/com/devzamse/tesis/view/Information.java
@@ -0,0 +1,16 @@
+package com.devzamse.tesis.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devzamse.tesis.R;
+
+public class Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
---
+++ app/src/main/java/com/devzamse/tesis/view/SearchProduct.java
@@ -0,0 +1,16 @@
+package com.devzamse.tesis.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devzamse.tesis.R;
+
+public class SearchProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_product);
+    }
+}
